fc92fa28d0969f5a0e360bf4a7398d58bdd563fa
900M100CK <chinhkhang105e@gmail.com>
2026-04-06 14:38:43 +0700

    fix new ticket

--- src/view/NewTicket.vue
+++ src/view/NewTicket.vue
@@ -1,3 +1,10 @@
+<template>
+  <div>
+    <h1>Tạo Yêu Cầu Mới</h1>
+    <helpdesk-form @createOrUpdate="createOrUpdate"></helpdesk-form>
+  </div>
+</template>
+
 <script>
 import HelpdeskForm from '../components/HelpdeskForm.vue';
 import { api } from '../helper/helpers';
