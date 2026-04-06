e7c6008477846804d59cc3f920c3d617cc635796
900M100CK <chinhkhang105e@gmail.com>
2026-04-06 14:31:59 +0700

    fix new ticket

--- src/view/NewTicket.vue
+++ src/view/NewTicket.vue
@@ -1,10 +1,3 @@
-<template>
-  <div>
-    <h1>Tạo Yêu Cầu Mới</h1>
-    <helpdesk-form @createOrUpdate="createOrUpdate"></helpdesk-form>
-  </div>
-</template>
-
 <script>
 import HelpdeskForm from '../components/HelpdeskForm.vue';
 import { api } from '../helper/helpers';
@@ -15,8 +8,14 @@ export default {
   methods: {
     createOrUpdate: async function(ticket) {
       const res = await api.createTicket(ticket);
+
+      // res có thể undefined nếu request lỗi (handleError chỉ log, không throw)
+      if (!res || !res.success) {
+        alert('Tạo ticket thất bại, kiểm tra lại kết nối!');
+        return;
+      }
+
       alert('Tạo ticket thành công!');
-      // Điều hướng về màn hình chi tiết
       this.$router.push(`/tickets/${res.data._id}`);
     }
   }
