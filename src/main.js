071a32c82ceed93bf603dc7bb9604390e3df35be
900M100CK <135854466+900M100CK@users.noreply.github.com>
2026-04-14 11:15:31 +0700

    Fix router import in main.js
    Updated router import statement in main.js.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import router from './router/router.js'; // Cần nâng cấp router lên v4
+import router from './router/router.js';
 import 'semantic-ui-css/semantic.min.css';
 
 createApp(App).use(router).mount('#app');
